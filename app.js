3d31d561ea29352c0b536944760eed38a7290eb0
dlashinsky <da.lashinsky@gmail.com>
2021-02-09 17:26:40 -0800

    pushing changes to da hub

--- app.js
+++ app.js
@@ -20,7 +20,7 @@
     
 
 
-// Point System (based on guesses)  Stored as Variables with Conditional Statements?
+// Point System (based on guesses)  Written as Conditional Statements
     // guess value === real value ---> 55pts
     // 5% or less distance to real value ---> 35 pts
     // > 5.1% || < 10% distance to real value ---> 20 pts
@@ -43,9 +43,12 @@
     let guessPriceEl = document.querySelector("#guess-price-txt")
     let guessratingEl = document.querySelector("#guess-rating-txt")
     let guessWeightEl = document.querySelector("#guess-weight-txt")
-    let guessPriceButtonEl = document.querySelector("#price-guess-button")    
+    let priceInputDiv = document.querySelector(".price-input-div")    
+    let guessPriceButtonEl = document.querySelector("#price-guess-button")
+    let ratingInputDiv = document.querySelector(".rating-input-div")
     let guessRatingButtonEl = document.querySelector("#rating-guess-button")    
-    let guessWeightButtonEl = document.querySelector("#weight-guess-button")    
+    let guessWeightButtonEl = document.querySelector("#weight-guess-button") 
+    let weightInputDiv = document.querySelector(".weight-input-div")   
     let userWinEl = document.querySelector("#user-win-msg")
     let compWinEl = document.querySelector("comp-win-msg")
     
@@ -139,19 +142,21 @@ function initializeGame () {
     prodCategoryEl.innerText= products[randomProductIndex].category
 
           
-  //Activating the game button to start on price 
+  //Activating the game button to start on price & Console logging the actual price of products for coding purposes DELETE when done
 
-   realPrice = products[randomProductIndex].price;
-        console.log(`The real price is ${realPrice}`)
-   realRating = products[randomProductIndex].rating;
-        console.log(`The real rating is ${realRating}`)
-   realWeight = products[randomProductIndex].weight;
-        console.log(`The real weight is ${realWeight}`)
+  
+    realPrice = products[randomProductIndex].price;
+        console.log("This is the actual price " + realPrice)
+    realRating = products[randomProductIndex].rating
+        console.log("This is the actual rating " + realRating)
+    realWeight = products[randomProductIndex].weight
+        console.log("This is the actual weight " + realWeight)
+
+    guessPriceButtonEl.addEventListener('click', evalPlayerPriceGuess);
 
 
-    guessPriceButtonEl.addEventListener('click', evaluateAnswers);
 
-            
+
     // Step 3: 
     // Inputting the game score values onto the page. 
     computerScoreEl.innerText = computerScore
@@ -160,188 +165,179 @@ function initializeGame () {
     //Step 4: Inputting what game round it is (Further rounds strech goal)
     gameRoundEl.innerText = currentRound
             
-};
+};  /////END intializeGame Func Here////
         
+function evalPlayerPriceGuess () {
 
-function evaluateAnswers (realPrice, realRating, realWeight) {
+///////////PRICE EVALUATIONS!!!!!////////////////
 
-///////////PLAYER GUESSING ON PRICE////////////////
+    userGuessPrice = guessPriceEl.value;
 
-userGuessPrice = guessPriceEl.value;
-realPrice = products[randomProductIndex].price;
-console.log("This is the second " + realPrice)
+    realPrice = products[randomProductIndex].price;
+        console.log("This is the second " + realPrice)
 
-let userPriceGuessPercent = Math.floor((((realPrice - userGuessPrice) / realPrice) * 100))
+    let userPriceGuessPercent = Math.floor((((realPrice - userGuessPrice) / realPrice) * 100))
 
-console.log(userPriceGuessPercent)
-
-
-///////CONDITIONALS FOR HOW CLOSE THE USER GETS TO THE ANSWER//////////
-
-if(userGuessPrice > realPrice) {
-    console.log("You over bid!  Negative Points!!!")
-    playerScore -=20;
-    playerScoreEl.innerText = playerScore;
-    runCompGuess(realPrice);
-    // evaluateRating();
-}
-if (userPriceGuessPercent >.001  &&  userPriceGuessPercent <= 5) {
-    playerScore += 35;
-    playerScoreEl.innerText = playerScore;
-    console.log("the 35pt conditional is working")
-    runCompGuess(realPrice);
-    runCompGuess(realPrice);
-}
-
-if (userPriceGuessPercent >5  &&  userPriceGuessPercent <= 10) {
-    playerScore += 20;
-    console.log("the 20pt conditional is working")
-    playerScoreEl.innerText = playerScore;
-    runCompGuess(realPrice);
-    // evaluateRating();
-}
-if (userPriceGuessPercent >10  &&  userPriceGuessPercent <= 20) {
-    playerScore += 10;
-    console.log("the 10pt conditional is working");
-    playerScoreEl.innerText = playerScore;
-    runCompGuess(realPrice); 
-    // evaluateRating();
-}
-if (userPriceGuessPercent >20  &&  userPriceGuessPercent <= 30) {
-    playerScore += 5;
-    console.log("the 5pt conditional is working");
-    playerScoreEl.innerText = playerScore;
-    runCompGuess(realPrice);
-    // evaluateRating();
-}
-if (userPriceGuessPercent >30  &&  userPriceGuessPercent <= 45) {
-    playerScore += 1;
-    console.log("the 1 pt conditional is working");
-    playerScoreEl.innerText = playerScore; 
-    runCompGuess(realPrice);
-    // evaluateRating();
-}
-if (userPriceGuessPercent > 45) {
-    playerScore += 0;
-    console.log("the 0 pt conditional is working");
-    playerScoreEl.innerText = playerScore;
-    runCompGuess(realPrice); 
-    // evaluateRating();
-
-}
-
-    else if (userGuessPrice == realPrice) {
-        playerScore += 55;
-        console.log("the 55pt conditional is working")
-        playerScoreEl.innerText = playerScore;
-        runCompGuess(realPrice);
-        // evaluateRating();
-    }
+        console.log(userPriceGuessPercent)
 
 
-////////////CONDITIONALS ON COMPUTER GUESS, AND RANDOMIZATION OF COMPUTER GUESS////////////
+        ///////////////CONDITIONALS FOR HOW CLOSE THE USER GETS TO THE PRICEANSWER//////////
 
-function runCompGuess (realPrice) {
+        if(userGuessPrice > realPrice) {
+            console.log("You over bid!  Negative Points!!!")
+            playerScore -=20;
+            playerScoreEl.innerText = playerScore;
+            runCompPriceGuess(realPrice);
+        }
+        if (userPriceGuessPercent >.001  &&  userPriceGuessPercent <= 5) {
+            playerScore += 35;
+            playerScoreEl.innerText = playerScore;
+            console.log("the 35pt conditional is working")
+            runCompPriceGuess(realPrice);
+        }
+        if (userPriceGuessPercent >5  &&  userPriceGuessPercent <= 10) {
+            playerScore += 20;
+            console.log("the 20pt conditional is working")
+            playerScoreEl.innerText = playerScore;
+            runCompPriceGuess(realPrice);
+        }
+        if (userPriceGuessPercent >10  &&  userPriceGuessPercent <= 20) {
+            playerScore += 10;
+            console.log("the 10pt conditional is working");
+            playerScoreEl.innerText = playerScore;
+            runCompPriceGuess(realPrice); 
+        }
+        if (userPriceGuessPercent >20  &&  userPriceGuessPercent <= 30) {
+            playerScore += 5;
+            console.log("the 5pt conditional is working");
+            playerScoreEl.innerText = playerScore;
+            runCompPriceGuess(realPrice);
+        }
+        if (userPriceGuessPercent >30  &&  userPriceGuessPercent <= 45) {
+            playerScore += 1;
+            console.log("the 1 pt conditional is working");
+            playerScoreEl.innerText = playerScore; 
+            runCompPriceGuess(realPrice);
+        }
+        if (userPriceGuessPercent > 45) {
+            playerScore += 0;
+            console.log("the 0 pt conditional is working");
+            playerScoreEl.innerText = playerScore;
+            runCompPriceGuess(realPrice); 
+        }
+            else if (userGuessPrice == realPrice) {
+                playerScore += 55;
+                console.log("the 55pt conditional is working")
+                playerScoreEl.innerText = playerScore;
+                runCompPriceGuess(realPrice);
+            }
 
-let adjustedRealPrice =  realPrice * 1.10
-console.log("The adjusted Real Price is " + adjustedRealPrice)
+} /////END evalPlayerPriceGuess Func here
+
+
+
+function runCompPriceGuess (realPrice) {
+    ////////////RANDOMIZATION OF COMPUTER GUESS////////////
+
+    let adjustedRealPrice =  realPrice * 1.10
+        console.log("The adjusted Real Price is " + adjustedRealPrice)
 
     compGuessPrice = Math.floor(Math.random() * (adjustedRealPrice/2)) + (realPrice / 2)
         console.log("the computer guessed " + compGuessPrice)
-        
-        
-let compPriceGuessPercent = Math.floor((((realPrice - compGuessPrice) / realPrice) * 100))
-    console.log("The computer price guess is " + compPriceGuessPercent + " percent.")
+            
+            
+    let compPriceGuessPercent = Math.floor((((realPrice - compGuessPrice) / realPrice) * 100))
+        console.log("The computer price guess is " + compPriceGuessPercent + " percent.")
+    
+        //////////CONDITIONALS FOR COMP GUESS//////////////
 
-
-}
-
-
-if(userGuessPrice > realPrice) {
-    console.log("You over bid!  Negative Points!!!")
-    playerScore -=20
-    playerScoreEl.innerText = playerScore
-    // evaluateRating();
-}
-if (userPriceGuessPercent >.001  &&  userPriceGuessPercent <= 5) {
-    playerScore += 35
-    playerScoreEl.innerText = playerScore 
-    console.log("the 20pt conditional is working")
-    // evaluateRating();
-}
-
-if (userPriceGuessPercent >5  &&  userPriceGuessPercent <= 10) {
-    playerScore += 20
-    console.log("the 20pt conditional is working")
-    playerScoreEl.innerText = playerScore 
-    // evaluateRating();
-}
-if (userPriceGuessPercent >10  &&  userPriceGuessPercent <= 20) {
-    playerScore += 10
-    console.log("the 10pt conditional is working")
-    playerScoreEl.innerText = playerScore 
-    // evaluateRating();
-}
-if (userPriceGuessPercent >20  &&  userPriceGuessPercent <= 30) {
-    playerScore += 5
-    console.log("the 5pt conditional is working")
-    playerScoreEl.innerText = playerScore 
-    // evaluateRating();
-}
-if (userPriceGuessPercent >30  &&  userPriceGuessPercent <= 45) {
-    playerScore += 1
-    console.log("the 1 pt conditional is working")
-    playerScoreEl.innerText = playerScore 
-    // evaluateRating();
-}
-if (userPriceGuessPercent > 45) {
-    playerScore += 0
-    console.log("the 0 pt conditional is working")
-    playerScoreEl.innerText = playerScore 
-    // evaluateRating();
-
-}
-
-    else if (userGuessPrice == realPrice) {
-        playerScore += 55
-        console.log("the 55pt conditional is working")
-        playerScoreEl.innerText = playerScore 
-        // evaluateRating();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        function evaluateRating() {
-            userGuessRating = guessratingEl.value;
-
+        if(compGuessPrice > realPrice) {
+            console.log("The Computer over bid!  Negative Points!!!")
+            computerScore -=20
+            computerScoreEl.innerText = computerScore
+            evalPlayerRatingGuess();
+        }
+        if (compPriceGuessPercent >.001  &&  compPriceGuessPercent <= 5) {
+            computerScore += 35
+            computerScoreEl.innerText = computerScore 
+            console.log("the computer 35pt conditional is working")
+            evalPlayerRatingGuess();
         }
         
-        userGuessWeight = guessWeightEl.value;
-        let userRatingGuessPercent = (((realRating - userGuessRating) / realRating) * 100)
-        let userWeightGuessPercent = (((realWeight - userGuessWeight) / realWeight) * 100)
+        if (compPriceGuessPercent >5  &&  compPriceGuessPercent <= 10) {
+            computerScore += 20
+            console.log("the computer 20pt conditional is working")
+            computerScoreEl.innerText = computerScore 
+            evalPlayerRatingGuess();
+        }
+        if (compPriceGuessPercent >10  &&  compPriceGuessPercent <= 20) {
+            computerScore += 10
+            console.log("the computer 10pt conditional is working")
+            computerScoreEl.innerText = computerScore 
+            evalPlayerRatingGuess();
+        }
+        if (compPriceGuessPercent >20  &&  compPriceGuessPercent <= 30) {
+            computerScore += 5
+            console.log("the computer 5pt conditional is working")
+            computerScoreEl.innerText = computerScore 
+            evalPlayerRatingGuess();
+        }
+        if (compPriceGuessPercent >30  &&  compPriceGuessPercent <= 45) {
+            computerScore += 1
+            console.log("the computer 1pt conditional is working")
+            computerScoreEl.innerText = computerScore 
+            evalPlayerRatingGuess();
+        }
+        if (compPriceGuessPercent > 45) {
+            computerScore += 0
+            console.log("the computer 0pt conditional is working")
+            computerScoreEl.innerText = computerScore 
+            evalPlayerRatingGuess();
+            
+        }
+        
+            else if (compGuessPrice == realPrice) {
+                computerScore += 55
+                console.log("the computer 55pt conditional is working")
+                computerScoreEl.innerText = computerScore 
+                evalPlayerRatingGuess();
+            }
+
+} /////End runCompPriceGuess 
+
+
+function evalPlayerRatingGuess() {
+    
+    priceInputDiv.setAttribute("style", "display: none;");
+    guessPriceButtonEl.setAttribute("style", "display: none;");
+
+    ratingInputDiv.removeAttribute("id");
+    guessRatingButtonEl.removeAttribute("id")
+    
+
+    
+    
+    userGuessRating = guessratingEl.value;
+
+
+    
+
+
+
+}/////END evalPlayerRatingGuess func
+        
+
+
+
+        // userGuessWeight = guessWeightEl.value;
+        // let userRatingGuessPercent = (((realRating - userGuessRating) / realRating) * 100)
+        // let userWeightGuessPercent = (((realWeight - userGuessWeight) / realWeight) * 100)
 
 
 
 
 
-}
+/////Evaluate Answers Function Bracket here/////
 
 
 
